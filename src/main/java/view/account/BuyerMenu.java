01772b2b0d1338d8c215f62d4acba935f83b8cee
rastin1379 <rastin1379@gmail.com>
2020-05-13 18:29:51 +0430

    BuyerMenu editField function body!

--- src/main/java/view/account/BuyerMenu.java
+++ src/main/java/view/account/BuyerMenu.java
@@ -4,13 +4,18 @@ import controller.account.user.BuyerController;
 import model.account.Account;
 import view.Menu;
 
+import java.util.Scanner;
+
 public class BuyerMenu extends Menu {
     BuyerController buyerController;
+    Scanner scanner;
 
     public BuyerMenu() {
         super("BuyerMenu");
         setRegex();
+        setMethods();
         buyerController = BuyerController.getInstance();
+        scanner = new Scanner(System.in);
     }
 
     @Override
@@ -28,11 +33,27 @@ public class BuyerMenu extends Menu {
         System.out.println("Credit:       " + account.getCredit());
     }
 
+    public void editField(String field) {
+        if (!field.equalsIgnoreCase("name") && !field.equalsIgnoreCase("last name") &&
+            !field.equalsIgnoreCase("email") && !field.equalsIgnoreCase("phone number") &&
+            !field.equalsIgnoreCase("password") && !field.equalsIgnoreCase("username") &&
+            !field.equalsIgnoreCase("field")) {
+            System.out.println("Field is not valid!");
+        }
+        else {
+            System.out.println("Please insert the new field");
+            String context = scanner.nextLine().trim();
+            buyerController.editField(field, context);
+        }
+    }
+
     private void setRegex() {
         regex.add("view personal info");
+        regex.add("edit (\\w+)");
     }
 
     private void setMethods() {
         methods.add("viewPersonalInfo");
+        methods.add("editField");
     }
 }
