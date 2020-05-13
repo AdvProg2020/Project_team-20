e39bf2111f1e7c780f87d7bd957aad260cb0a9e2
panizhalvachi <panizhalvachi@gmail.com>
2020-05-13 19:21:38 +0430

    start ManagerMenu!

--- src/main/java/view/account/ManagerMenu.java
+++ src/main/java/view/account/ManagerMenu.java
@@ -1,4 +1,23 @@
 package view.account;
 
-public class ManagerMenu {
+import view.Menu;
+
+public class ManagerMenu extends Menu {
+
+    public ManagerMenu(String name) {
+        super(name);
+        this.regex.add(0,"view personal info");
+        this.methods.add(0,"viewPersonalInfo");
+
+    }
+
+    public void viewPersonalInfo(){
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
 }
