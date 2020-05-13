1fa5431ca2704570a1d8fbf0f95cd0942089b4f3
panizhalvachi <panizhalvachi@gmail.com>
2020-05-13 20:23:27 +0430

    add viewPersonalInfo and editPersonalInfo and manageUsers to ManagerMenu(just for sure)

--- src/main/java/view/account/ManagerMenu.java
+++ src/main/java/view/account/ManagerMenu.java
@@ -1,8 +1,11 @@
 package view.account;
 
+import controller.account.user.ManagerController;
 import model.account.Account;
 import model.account.Manager;
 import view.Menu;
+
+import java.util.ArrayList;
 
 public class ManagerMenu extends Menu {
 
@@ -12,21 +15,40 @@ public class ManagerMenu extends Menu {
         super("ManagerMenu");
         this.regex.add(0,"view personal info");
         this.methods.add(0,"viewPersonalInfo");
-
+        this.regex.add(1,"edit (\\w+)");
+        this.methods.add(1,"editPersonalInfo");
+        this.regex.add(2,"manage users");
+        this.methods.add(2,"manageUsers");
     }
-    public static ManagerMenu getInstance()
-    {
+
+    public static ManagerMenu getInstance() {
         if (single_instance == null)
             single_instance = new ManagerMenu();
 
         return single_instance;
     }
 
-    public void viewPersonalInfo(){
-
-        Account account =
+    public void viewPersonalInfo() {
+        ManagerController managerController = ManagerController.getInstance();
+        Account account = managerController.getAccountInfo();
+        System.out.println("UserName : " + account.getUsername() + "\n" + "Password : " + account.getPassword() + "\n" + "Name : " + account.getName() + "\n" + "LastName : " + account.getLastName() + "\n" + "Email : " + account.getEmail()
+                + "\n" + "PhoneNumber : " + account.getPhoneNumber());
     }
 
+    public void editPersonalInfo(String field){
+        System.out.println("please write new field : ");
+        String context = Menu.scanner.nextLine();
+        ManagerController managerController = ManagerController.getInstance();
+        managerController.editField(field , context);
+    }
+
+    public void manageUsers(){
+        ManagerController managerController = ManagerController.getInstance();
+        ArrayList<Account> accounts = managerController.manageUsers();
+        for(Account account : accounts){
+            System.out.println("UserName : "+account.getUsername());
+        }
+    }
 
     @Override
     public void show() {
