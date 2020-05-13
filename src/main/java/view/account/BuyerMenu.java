343d913152c672f0b9434a9b8c08c798c6177d8a
rastin1379 <rastin1379@gmail.com>
2020-05-13 18:15:51 +0430

    BuyerMenu personalInfo function body!

--- src/main/java/view/account/BuyerMenu.java
+++ src/main/java/view/account/BuyerMenu.java
@@ -1,4 +1,38 @@
 package view.account;
 
-public class BuyerMenu {
+import controller.account.user.BuyerController;
+import model.account.Account;
+import view.Menu;
+
+public class BuyerMenu extends Menu {
+    BuyerController buyerController;
+
+    public BuyerMenu() {
+        super("BuyerMenu");
+        setRegex();
+        buyerController = BuyerController.getInstance();
+    }
+
+    @Override
+    public void show() {
+    }
+
+    public void viewPersonalInfo() {
+        Account account = buyerController.getAccountInfo();
+        System.out.println("Name:         " + account.getName());
+        System.out.println("LastName:     " + account.getLastName());
+        System.out.println("Email:        " + account.getEmail());
+        System.out.println("Phone number: " + account.getPhoneNumber());
+        System.out.println("Username:     " + account.getUsername());
+        System.out.println("Password:     " + account.getPassword());
+        System.out.println("Credit:       " + account.getCredit());
+    }
+
+    private void setRegex() {
+        regex.add("view personal info");
+    }
+
+    private void setMethods() {
+        methods.add("viewPersonalInfo");
+    }
 }
