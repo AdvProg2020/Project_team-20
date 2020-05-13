9a916b2692a81684412af0dca898711a8fd3506b
rastin1379 <rastin1379@gmail.com>
2020-05-14 01:42:48 +0430

    AllProductsMenu without sorting & filtering!

--- src/main/java/view/product/AllProductsMenu.java
+++ src/main/java/view/product/AllProductsMenu.java
@@ -1,9 +1,56 @@
 package view.product;
 
+import controller.product.filter.AllProductsController;
+import model.product.Category;
+import model.product.Product;
+import model.product.SelectedProduct;
 import view.Menu;
+import view.account.BuyerMenu;
+
+import java.util.ArrayList;
 
 public class AllProductsMenu extends Menu {
-    public AllProductsMenu(String name, Menu parent) {
-        super(name, parent);
+    AllProductsController allProductsController;
+
+   private static AllProductsMenu allProductsMenu = null;
+
+    private AllProductsMenu() {
+        super("allProductsMenu");
+        allProductsController = AllProductsController.getInstance();
     }
+
+    public static AllProductsMenu getInstance() {
+        if (allProductsMenu == null)
+            allProductsMenu = new AllProductsMenu();
+        return allProductsMenu;
+    }
+
+    //NOT SURE!!!!!!!!!!
+    public void viewCategories() {
+        ArrayList<Category> categories = allProductsController.getAllCategories();
+        System.out.println("All categories:");
+        for (Category category:categories)
+            System.out.println(category.getName());
+    }
+
+    public void showProducts() {
+        ArrayList<Product> products = allProductsController.getAllProducts();
+        for (Product product:products) {
+            System.out.println("Name                id\n");
+            System.out.format("%s%20s", product.getName()
+                    , product.getId());
+        }
+    }
+
+
+    private void setRegex() {
+        regex.add("view categories");
+        regex.add("show products");
+    }
+
+    private void setMethods() {
+        methods.add("viewCategories");
+        methods.add("showProducts");
+    }
+
 }
