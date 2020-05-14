2fd2062ef0f2e62139a302dc6728b05ba0feb476
rastin1379 <rastin1379@gmail.com>
2020-05-15 01:19:57 +0430

    Filterable 3.5

--- src/main/java/controller/product/filter/Filterable.java
+++ src/main/java/controller/product/filter/Filterable.java
@@ -9,6 +9,7 @@ import model.product.Field.NumericalField;
 import model.product.Product;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public abstract class Filterable {
     protected ArrayList<Filter> filters = new ArrayList<>();
@@ -28,6 +29,17 @@ public abstract class Filterable {
         else if (filterType.equalsIgnoreCase("numerical field")) {
             filterByNumericalFilter(details);
         }
+    }
+
+    public ArrayList<Product> getProducts () {
+        return productsToShow.stream()
+                .filter( product -> {
+                    for (Filter filter:filters)
+                        if (!filter.validFilter(product))
+                            return false;
+                    return true;})
+                .collect(Collectors
+                .toCollection(ArrayList::new));
     }
 
     public void filterByCategory(ArrayList<String> details) throws Exception{
