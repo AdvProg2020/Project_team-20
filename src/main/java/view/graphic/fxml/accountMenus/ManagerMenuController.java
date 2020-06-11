72c5ec49567d9e8eb34d8dc2a56ca37f012ee293
rastin1379 <rastin1379@gmail.com>
2020-06-11 22:56:37 +0430

    Manager menu 1!

--- src/main/java/view/graphic/fxml/accountMenus/ManagerMenuController.java
+++ src/main/java/view/graphic/fxml/accountMenus/ManagerMenuController.java
@@ -1,7 +1,20 @@
 package view.graphic.fxml.accountMenus;
 
-public class ManagerMenuController {
-    public void start(){
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.File;
+
+public class ManagerMenuController {
+    private static Stage window;
+
+    public void start(Stage mainStage) throws Exception{
+        window = mainStage;
+        Parent root = FXMLLoader.load(new File("src/main/java/view/graphic/fxml/registerAndLoginMenu/SignUpSeller.fxml").toURI().toURL());
+        window.setTitle("Sign up menu");
+        window.setScene(new Scene(root, 994, 666));
+        window.show();
     }
 }
