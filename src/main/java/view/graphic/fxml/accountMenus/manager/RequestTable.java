1dbb9d5dd331952f8bc40c2dd0bda244828c9ca9
panizhalvachi <panizhalvachi@gmail.com>
2020-06-13 11:59:40 +0430

    requesttttttttt

---
+++ src/main/java/view/graphic/fxml/accountMenus/manager/RequestTable.java
@@ -0,0 +1,21 @@
+package view.graphic.fxml.accountMenus.manager;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class RequestTable {
+    private SimpleStringProperty detail;
+    private SimpleStringProperty id;
+
+    public RequestTable(String detail , int id){
+        this.detail = new SimpleStringProperty(detail);
+        this.id = new SimpleStringProperty(Integer.toString(id));
+    }
+
+    public String getDetail() {
+        return detail.get();
+    }
+
+    public String getId(){
+        return id.get();
+    }
+}
