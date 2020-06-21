907bfdfc52cc6fa7be294bcdb7580d06b3185d48
rastin1379 <rastin1379@gmail.com>
2020-06-21 19:49:07 +0430

    All Products menu!

--- src/main/java/view/graphic/fxml/allProductsMenu/FxmlAllProductsMenu.java
+++ src/main/java/view/graphic/fxml/allProductsMenu/FxmlAllProductsMenu.java
@@ -16,6 +16,7 @@ import view.graphic.MenuNames;
 import view.graphic.ProgramApplication;
 import view.graphic.alert.AlertController;
 import view.graphic.alert.AlertType;
+import view.graphic.score.Score;
 
 import java.io.File;
 import java.net.URL;
@@ -24,6 +25,24 @@ import java.util.ResourceBundle;
 
 public class FxmlAllProductsMenu implements Initializable {
     public Pane categories;
+    public Text product1Price;
+    public ImageView product1Score;
+    public Text product2Price;
+    public ImageView product2Score;
+    public Text product3Price;
+    public ImageView product3Score;
+    public Text product4Price;
+    public ImageView product4Score;
+    public Text product5Price;
+    public ImageView product5Score;
+    public Text product6Price;
+    public ImageView product6Score;
+    public Text product7Price;
+    public ImageView product7Score;
+    public Text product8Price;
+    public ImageView product8Score;
+    public Text product9Price;
+    public ImageView product9Score;
     private AllProductsController allProductsController = AllProductsController.getInstance();
     private static ArrayList<Product> products;
 
@@ -84,49 +103,67 @@ public class FxmlAllProductsMenu implements Initializable {
     }
 
     private void initializeProducts(int from) {
-        System.out.println(products.get(from).getImagePath());
         Image img1 = new Image(new File("src/main/resources/Images/" + products.get(from).getImagePath()).toURI().toString());
         productImg1.setImage(img1);
         product1.setText(products.get(from).getName());
+        product1Price.setText(Double.toString(products.get(from).getFirstPrice()));
+        product1Score.setImage(new Score(products.get(from).getAverage()).getScoreImg());
+
         if (products.size()>(1+from)) {
             Image img2 = new Image(new File("src/main/resources/Images/" + products.get(1 + from).getImagePath()).toURI().toString());
             productImg2.setImage(img2);
             product2.setText(products.get(1 + from).getName());
+            product2Price.setText(Double.toString(products.get(1 + from).getFirstPrice()));
+            product2Score.setImage(new Score(products.get(1 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(2+from)) {
             Image img3 = new Image(new File("src/main/resources/Images/" + products.get(2 + from).getImagePath()).toURI().toString());
             productImg3.setImage(img3);
             product3.setText(products.get(2 + from).getName());
+            product3Price.setText(Double.toString(products.get(2 + from).getFirstPrice()));
+            product3Score.setImage(new Score(products.get(2 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(3+from)) {
             Image img4 = new Image(new File("src/main/resources/Images/" + products.get(3 + from).getImagePath()).toURI().toString());
             productImg4.setImage(img4);
             product4.setText(products.get(3 + from).getName());
+            product4Price.setText(Double.toString(products.get(3 + from).getFirstPrice()));
+            product4Score.setImage(new Score(products.get(3 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(4+from)) {
             Image img5 = new Image(new File("src/main/resources/Images/" + products.get(4 + from).getImagePath()).toURI().toString());
             productImg5.setImage(img5);
             product5.setText(products.get(4 + from).getName());
+            product5Price.setText(Double.toString(products.get(4 + from).getFirstPrice()));
+            product5Score.setImage(new Score(products.get(4 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(5+from)) {
             Image img6 = new Image(new File("src/main/resources/Images/" + products.get(5 + from).getImagePath()).toURI().toString());
             productImg6.setImage(img6);
             product6.setText(products.get(5 + from).getName());
+            product6Price.setText(Double.toString(products.get(5 + from).getFirstPrice()));
+            product6Score.setImage(new Score(products.get(5 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(6+from)) {
             Image img7 = new Image(new File("src/main/resources/Images/" + products.get(6 + from).getImagePath()).toURI().toString());
             productImg7.setImage(img7);
             product7.setText(products.get(6 + from).getName());
+            product7Price.setText(Double.toString(products.get(6 + from).getFirstPrice()));
+            product7Score.setImage(new Score(products.get(6 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(7+from)) {
             Image img8 = new Image(new File("src/main/resources/Images/" + products.get(7 + from).getImagePath()).toURI().toString());
             productImg8.setImage(img8);
             product8.setText(products.get(7 + from).getName());
+            product8Price.setText(Double.toString(products.get(7 + from).getFirstPrice()));
+            product8Score.setImage(new Score(products.get(7 + from).getAverage()).getScoreImg());
         }
         if (products.size()>(8+from)) {
             Image img9 = new Image(new File("src/main/resources/Images/" + products.get(8 + from).getImagePath()).toURI().toString());
             productImg9.setImage(img9);
             product9.setText(products.get(8 + from).getName());
+            product9Price.setText(Double.toString(products.get(8 + from).getFirstPrice()));
+            product9Score.setImage(new Score(products.get(8 + from).getAverage()).getScoreImg());
         }
     }
 
