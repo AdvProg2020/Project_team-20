143bc71464ccc0a47f9bd96a68d877e40aa941ef
rastin1379 <rastin1379@gmail.com>
2020-06-22 10:42:50 +0430

    SellerMenu offs

--- src/main/java/view/graphic/fxml/accountMenus/seller/SellerMenuController.java
+++ src/main/java/view/graphic/fxml/accountMenus/seller/SellerMenuController.java
@@ -6,23 +6,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import model.product.Product;
+import model.account.Seller;
 import view.graphic.MenuNames;
 import view.graphic.ProgramApplication;
 import view.graphic.alert.AlertController;
 import view.graphic.alert.AlertType;
+
 import java.io.File;
-import java.util.ArrayList;
 
 public class SellerMenuController {
     private static Stage window;
     public BorderPane borderPane;
     private SellerController sellerController = SellerController.getInstance();
+    private Seller seller = (Seller) sellerController.getAccountInfo();
 
     public static void start(Stage stage) throws Exception {
         window = stage;
@@ -41,13 +40,7 @@ public class SellerMenuController {
     }
 
     public void handleManageProducts(ActionEvent actionEvent) {
-        loadUI("manageProducts");
-        ArrayList<Product> products = SellerController.getInstance().getAllProducts();
-        for (Product product:products){
-            ImageView imageView = new ImageView();
-            Image image = new Image(product.getImagePath());
-            imageView.setImage(image);
-        }
+
     }
 
     public void handleShowCategories(ActionEvent actionEvent) {
@@ -94,5 +87,9 @@ public class SellerMenuController {
 
     public void handleCreateProduct(ActionEvent actionEvent) {
         loadUI("createProduct");
+    }
+
+    public void handleCreateOff(ActionEvent actionEvent) {
+        loadUI("createOff");
     }
 }
