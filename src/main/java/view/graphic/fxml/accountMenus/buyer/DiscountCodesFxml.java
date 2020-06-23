ac0f50d53431a5dd8f6a03936cc450930a30c773
panizhalvachi <panizhalvachi@gmail.com>
2020-06-23 10:30:56 +0430

    add view discount codes to buyer menu

---
+++ src/main/java/view/graphic/fxml/accountMenus/buyer/DiscountCodesFxml.java
@@ -0,0 +1,26 @@
+package view.graphic.fxml.accountMenus.buyer;
+
+import controller.account.user.BuyerController;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import model.product.Discount;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class DiscountCodesFxml implements Initializable {
+
+    public TextArea textArea;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        BuyerController buyerController = BuyerController.getInstance();
+        ArrayList<Discount> discounts = buyerController.getAllDiscounts();
+        for(Discount discount : discounts){
+            String line = "discount code: "+discount.getDiscountCode()+"\n"+"discount percentage: "+discount.getDiscountPercentage()+"\n";
+            textArea.appendText(line);
+        }
+    }
+}
