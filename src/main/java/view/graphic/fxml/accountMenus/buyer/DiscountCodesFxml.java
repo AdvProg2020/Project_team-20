0ad6fb489f096668935c51fe519abac587a1e0ff
rastin1379 <rastin1379@gmail.com>
2020-06-23 10:38:23 +0430

    Discount fixed!

--- src/main/java/view/graphic/fxml/accountMenus/buyer/DiscountCodesFxml.java
+++ src/main/java/view/graphic/fxml/accountMenus/buyer/DiscountCodesFxml.java
@@ -19,7 +19,7 @@ public class DiscountCodesFxml implements Initializable {
         BuyerController buyerController = BuyerController.getInstance();
         ArrayList<Discount> discounts = buyerController.getAllDiscounts();
         for(Discount discount : discounts){
-            String line = "discount code: "+discount.getDiscountCode()+"\n"+"discount percentage: "+discount.getDiscountPercentage()+"\n";
+            String line = "discount code: "+discount.getDiscountCode()+"\n"+"discount percentage: "+discount.getDiscountPercentage()+"\n" + "\n";
             textArea.appendText(line);
         }
     }
