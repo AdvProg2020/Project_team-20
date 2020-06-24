57cc5a11ba03bcc0d969601264e6678e596f08f9
rastin1379 <rastin1379@gmail.com>
2020-06-24 23:21:09 +0430

    password field added

--- src/main/java/view/graphic/fxml/registerAndLoginMenu/FxmlRegisterAndLoginMenu.java
+++ src/main/java/view/graphic/fxml/registerAndLoginMenu/FxmlRegisterAndLoginMenu.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -27,7 +28,7 @@ public class FxmlRegisterAndLoginMenu {
     private static Stage window;
     private static String signUpMode = "b";
     public TextField newUsername;
-    public TextField newPassword;
+    public PasswordField newPassword;
     public TextField newEmail;
     public TextField newName;
     public TextField newLastName;
