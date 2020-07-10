029397c0fa652f288335cb3b3a02c228c5181ebe
Ehsan200 <59165228+Ehsan200@users.noreply.github.com>
2020-07-10 13:20:38 +0430

    edit AuthToken write Message

--- src/main/java/network/AuthToken.java
+++ src/main/java/network/AuthToken.java
@@ -12,27 +12,16 @@ public class AuthToken {
     }
 
 
-    public static AuthToken generateAuth(String username){
-        int i=0;
-        for (Account acc : Account.getAllAccounts()) {
-            if(acc.getUsername().equals(username))break;
-            i++;
-        }
-        StringBuilder toBeHashed = new StringBuilder();
-        int key = toBeHashed.append(i).append(username).toString().hashCode();
-        System.out.println("key = " + key);
-        return new AuthToken(username, key);
+   /* public static AuthToken generateAuth(String username){
+
     }
 
     //is valid
     public boolean authenticate() {
-        int i = 0;
-        for (Account account : Account.getAllAccounts()) {
-            if (account.getUsername().equals(this.getUsername())) break;
-            i++;
-        }
-        return (i + this.getUsername()).hashCode() == this.getKey();
+
     }
+
+    */
 
 
     public int getKey() {
--- src/main/java/network/Message.java
+++ src/main/java/network/Message.java
@@ -1,4 +1,47 @@
 package network;
 
+import java.util.ArrayList;
+
 public class Message {
+    private String text;
+    private ArrayList<Object> objects = new ArrayList<>();
+    private AuthToken authToken = null;
+
+    public Message(String text, AuthToken authToken) {
+        this.text = text;
+        this.authToken = authToken;
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public AuthToken getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuth(AuthToken authToken) {
+        this.authToken = authToken;
+    }
+
+    public void addToObjects(Object carry) {
+        this.objects.add(carry);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public ArrayList<Object> getObjects() {
+        return objects;
+    }
+
+    public static Message getFailedMessage() {
+        return new Message("failed!");
+    }
+
+    public static Message getDoneMessage() {
+        return new Message("Done!");
+    }
+
 }
