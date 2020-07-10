1083450d81e51494852572633a73b64149187070
rastin1379 <rastin1379@gmail.com>
2020-07-10 19:58:22 +0430

    BankServer3

--- src/main/java/server/controller/bank/BankServer.java
+++ src/main/java/server/controller/bank/BankServer.java
@@ -1,10 +1,17 @@
 package server.controller.bank;
 
+import server.model.bank.BankAccount;
+import server.network.Message;
 import server.network.server.Server;
 
+import java.util.ArrayList;
+
 public class BankServer extends Server {
+    ArrayList<BankAccount> bankAccounts;
+
     public BankServer() {
         super(9000);
+        bankAccounts = new ArrayList<>();
     }
 
     @Override
@@ -12,7 +19,15 @@ public class BankServer extends Server {
         methods.add("createAccount");
     }
 
-    public void createAccount(String username, String password) {
+    public Message createAccount(String username, String password) {
+        return null;
+    }
 
+    private BankAccount getBankAccountByUsername(String username) {
+        for (BankAccount bankAccount:bankAccounts) {
+            if (bankAccount.getUsername().equals(username))
+                return bankAccount;
+        }
+        return null;
     }
 }
