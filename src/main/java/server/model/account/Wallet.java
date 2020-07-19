aa54f7752cb9319514654090682e4d913baffd9b
rastin1379 <rastin1379@gmail.com>
2020-07-19 15:29:52 +0430

    pictures changed!

--- src/main/java/server/model/account/Wallet.java
+++ src/main/java/server/model/account/Wallet.java
@@ -3,11 +3,27 @@ package server.model.account;
 public class Wallet {
     double money;
 
+    public void increaseMoney(double moneyToAdd) {
+        money+=moneyToAdd;
+    }
+
+    public void decreaseMoney(double moneyToSub) throws Exception{
+        if (money<moneyToSub)
+            throw new NotEnoughMoney();
+        money-=moneyToSub;
+    }
+
     public Wallet(double money) {
         this.money = money;
     }
 
     public double getMoney() {
         return money;
+    }
+
+    public static class NotEnoughMoney extends Exception {
+        public NotEnoughMoney() {
+            super("Not Enough money");
+        }
     }
 }
