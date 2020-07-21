a2cf9e33d1f9e22287cba1e1dc06794a0637c204
rastin1379 <rastin1379@gmail.com>
2020-07-21 23:13:03 +0430

    Secure4

--- src/main/java/server/network/security/DangerousIp.java
+++ src/main/java/server/network/security/DangerousIp.java
@@ -4,16 +4,21 @@ import java.time.LocalDateTime;
 
 public class DangerousIp {
     private LocalDateTime timeWentDangerous;
+    private DangerousIpType dangerousIpType;
     private String ip;
 
-    public DangerousIp(String ip) {
+    public DangerousIp(String ip, DangerousIpType dangerousIpType) {
         this.timeWentDangerous = LocalDateTime.now();
         this.ip = ip;
     }
 
     public boolean isStillDangerous() {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime after = timeWentDangerous.plusMinutes(3);
+        LocalDateTime after;
+        if (dangerousIpType.equals(DangerousIpType.LOGIN_DANGER))
+            after = timeWentDangerous.plusMinutes(3);
+        else
+            after = timeWentDangerous.plusMinutes(30);
         return !now.isAfter(after);
     }
 
