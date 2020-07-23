35b4ba22dbbe372e09667a333079828da77736a9
rastin1379 <rastin1379@gmail.com>
2020-07-24 01:28:03 +0430

    File Graph 4

--- src/main/java/client/view/graphic/fxml/accountMenus/seller/SellerMenuController.java
+++ src/main/java/client/view/graphic/fxml/accountMenus/seller/SellerMenuController.java
@@ -140,8 +140,6 @@ public class SellerMenuController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Seller seller = SellerController.getSeller();
-        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         if (seller.getImagePath() != null) {
             Image img1 = new Image(new File("src/main/resources/Images/" + seller.getImagePath()).toURI().toString());
             profileImg.setImage(img1);
